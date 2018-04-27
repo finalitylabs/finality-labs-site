07644955be4718f94d9d70f90039df2b98fcbb46
Jonathan Dunlap <jdunlap@outlook.com>
2018-04-27 16:24:27 -0700

    banner readded

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../assets/Logos/FL_Logo.png';
-
+import lab from '../assets/lab-apng.png';
 
 
 
@@ -9,6 +9,7 @@ class Main extends Component {
 
         return (
             <div id='Main'className='main'>
+                <img src={lab} alt='Finality-Labs-logo' width='754px' height='526px'/>
                 <div className='logo'>
                     <img src={logo} alt='Finality-Labs-logo' width='150px'/>
                 </div>
