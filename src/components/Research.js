8e5955c76844028623da4f79e27e8d7559924978
Jonathan Dunlap <jdunlap@outlook.com>
2018-04-27 21:15:58 -0700

    remove height vh on page elements to fix mobile view

--- src/components/Research.js
+++ src/components/Research.js
@@ -4,7 +4,7 @@ class Research extends Component {
     render() {
         return (
             <div id='Research' className='research'>
-                <div>
+                <div className='works'>
                     <h1>Research</h1>
                     <p>Gaming on the blockchain will not work without immediate solutions to how these system will be ran in a cost-effective and decentralized way. A core component of Finality Labs is the research and development done into scaling Ethereum and other blockchain ledgers. Finality Labs is collaborating with SpankChain on their generalized-state-channels implementation to facilitate cheap channel instantiation, state transfers, and channel upgrade paths for both the SpankChain camsite and the CryptoKitties Battle Arena while an RPG in the works will explore an application-specific plasma chain.</p>
                 </div>
