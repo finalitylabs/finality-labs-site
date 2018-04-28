bf707628d712ecf20065840c1d22e5f1a4a8b202
Will Vatcher <will.vatcher@gmail.com>
2018-04-27 23:11:57 -0700

    Restructured Style

--- src/components/Join.js
+++ src/components/Join.js
@@ -6,11 +6,28 @@ import github from '../assets/Social/github.png'
 import facebook from '../assets/Social/facebook.png'
 import email from '../assets/Social/email.png'
 import discord from '../assets/Social/discord.png'
+import sc from '../assets/Partners/SpankChain_Logo.png';
+import dzx from '../assets/Partners/district0X_Logo.png';
+import dl from '../assets/Partners/Decentraland_Logo.png';
+import az from '../assets/Partners/AxiomZen_Logo.png';
 
 class Join extends Component {
     render() {
         return (
             <div id='Join' className='join-outer'>
+                <div className='partners-outer'>
+                    <h1>Partnerships</h1>
+                    <div className='partners'>
+                        <a href='https://spankchain.com/' rel="noopener noreferrer" target='_blank'>
+                            <img src={sc} alt ='' width='200px' /></a>
+                        <a href='https://district0x.io/' rel="noopener noreferrer" target='_blank'>
+                            <img src={dzx} alt ='' width='200px' /></a>
+                        <a href='https://decentraland.org/' rel="noopener noreferrer" target='_blank'>
+                            <img src={dl} alt ='' width='200px' /></a>
+                        <a href='https://www.axiomzen.co/' rel="noopener noreferrer" target='_blank'>
+                            <img src={az} alt ='' width='200px' /></a>
+                    </div>
+                </div>
                 <div className='join'>
                     <div>
                         <a href='https://twitter.com/finalitylabs' rel="noopener noreferrer" target='_blank'>
--- src/components/Team.js
+++ src/components/Team.js
@@ -1,8 +1,4 @@
 import React, { Component } from 'react';
-import sc from '../assets/Partners/SpankChain_Logo.png';
-import dzx from '../assets/Partners/district0X_Logo.png';
-import dl from '../assets/Partners/Decentraland_Logo.png';
-import az from '../assets/Partners/AxiomZen_Logo.png';
 
 import nathan from '../assets/Team/Nathan.png';
 import tiffany from '../assets/Team/Tiffany.png';
@@ -36,11 +32,11 @@ class Team extends Component {
                     <div className='card'>
                         <img src={will} className='image clip' width='150px' />
                         <h3>Will Vatcher</h3>
-                        <p>Web Laborer</p>
+                        <p>Web Slinger</p>
                     </div>
 
                 </div>
-                <div className='team'>
+                <div className='team-2'>
                     <div className='card'>
                         <img src={adrian} className='image clip' width='150px' />
                         <h3>Adrian Karpenkopf</h3>
@@ -55,19 +51,6 @@ class Team extends Component {
                         <img src={james} className='image clip' width='200px' />
                         <h3>James Young</h3>
                         <p>Advisor</p>
-                    </div>
-                </div>
-                <div className='partners-outer'>
-                    <h2>Partnerships</h2>
-                    <div className='partners'>
-                        <a href='https://spankchain.com/' rel="noopener noreferrer" target='_blank'>
-                            <img src={sc} alt ='' width='200px' /></a>
-                        <a href='https://district0x.io/' rel="noopener noreferrer" target='_blank'>
-                            <img src={dzx} alt ='' width='200px' /></a>
-                        <a href='https://decentraland.org/' rel="noopener noreferrer" target='_blank'>
-                            <img src={dl} alt ='' width='200px' /></a>
-                        <a href='https://www.axiomzen.co/' rel="noopener noreferrer" target='_blank'>
-                            <img src={az} alt ='' width='200px' /></a>
                     </div>
                 </div>
             </div>
