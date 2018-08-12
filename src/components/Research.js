31ae05168b1c53e9d6d370c47219e2f4d5b69ce8
nginnever <ginneversource@gmail.com>
2018-08-12 16:55:24 -0700

    update research

--- src/components/Research.js
+++ src/components/Research.js
@@ -36,10 +36,10 @@ class Research extends Component {
                             <a href={setPaymentChannelPDF} rel='noopener noreferrer' target="_blank" style={{background: 'white', color: '#6b5acd', border: 'none', padding: '10px', cursor: 'pointer', textDecoration: 'none'}}>View PDF</a>
                         </div>
                         <div className='pdf-right' >
-                            <h4>General State Channels</h4>
+                            <h4>State Channels and Plasma</h4>
                         
-                            <p>The Set-General paper outlines our protocol for arbitrarily complex turn based state systems to be ran over provider nodes.</p>
-                            <a href='#' rel='noopener noreferrer' style={{background: 'white', color: '#6b5acd', border: 'none', padding: '10px', cursor: 'pointer', textDecoration: 'none'}}>View PDF</a>
+                            <p>The Set-Plasma paper outlines our protocol for arbitrarily complex state systems to be ran over provider nodes.</p>
+                            <a href='https://docs.google.com/document/d/15LdH-YL3syBHdHlwCfUHou6XFvg5lXBKtrAFp5bG1Pc' rel='noopener noreferrer' style={{background: 'white', color: '#6b5acd', border: 'none', padding: '10px', cursor: 'pointer', textDecoration: 'none'}}>View PDF</a>
                         </div>
                     </div>
                 </Fade>
@@ -48,11 +48,6 @@ class Research extends Component {
                     duration={500}
                     distance='100px'
                 >
-                <div className='research-plasma'>
-                <h4>Plasma</h4>
-                
-                <p>We are prototyping an RPG that will explore an application-specific plasma chain. This requires non-UTXO based state challenges to allow exits to a parent chain. </p>
-                </div>
                 </Fade>
                 </div>
             </div>
