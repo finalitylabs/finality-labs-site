2e0a7b0dc49939f4fea5085d0bf95a300af39d36
nginnever <ginneversource@gmail.com>
2019-04-18 11:22:39 -0700

    ghpages sucks

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -18,7 +18,7 @@ class Mission extends Component {
                     
                         <h1>Mission</h1>
                         
-                        <p>Finality Labs is a community of researchers, engineers uniting to experiment with and create new applications of cryptographic systems. We build protocols, tools, games, and generally anything interesting. 
+                        <p>Finality Labs is a community of researchers and engineers uniting to experiment with and create new applications of cryptographic systems. We build protocols, tools, games, and generally anything interesting. 
                         We imagine and practically implement solutions to hard challenges in computational sciences. Finality Labs is a non-profit that strives to coordinate those that are most invested in the systems they build and allow them to decide how it should be governed.</p>
                     
 
